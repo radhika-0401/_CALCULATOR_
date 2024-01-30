de9a17ea17441eda7bf05e270a42628ae13d43f4
radhika-0401 <157567579+radhika-0401@users.noreply.github.com>
2024-01-30 14:23:48 +0530

    Add files via upload

---
+++ CALCULATOR/index.js
@@ -0,0 +1,28 @@
+//CALCULATOR PROGRAM
+
+const display=document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value ="";
+
+}
+
+function calculate(){
+
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value ="Error";
+    }
+
+}
+
+function del(){
+    display.value=display.value.slice(0,-1);
+}
